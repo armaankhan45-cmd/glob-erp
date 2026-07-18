b464245b25e21c4da2d8cd94a61f223050352596
armaankhan45-cmd <armaankhan87077@gmail.com>
2026-07-18 20:46:43 +0530

    Update sw.js

--- frontend/public/sw.js
+++ frontend/public/sw.js
@@ -1,15 +1,6 @@
-// Service Worker - NO-OP version
-// This unregisters any old broken service workers and does nothing else
-self.addEventListener('install', () => {
-  self.skipWaiting();
-});
-
-self.addEventListener('activate', (event) => {
-  event.waitUntil(
-    caches.keys().then(names => {
-      return Promise.all(names.map(name => caches.delete(name)));
-    }).then(() => clients.claim()).then(() => {
-      return self.registration.unregister();
-    })
+self.addEventListener('install', () => { self.skipWaiting(); });
+self.addEventListener('activate', (e) => {
+  e.waitUntil(
+    caches.keys().then(n => Promise.all(n.map(k => caches.delete(k)))).then(() => clients.claim()).then(() => self.registration.unregister())
   );
 });
