0775af7ff03f1524b1732c4407b5dc4baf5abf76
armaankhan45-cmd <armaankhan87077@gmail.com>
2026-07-18 20:51:27 +0530

    Delete frontend/public/sw.js

--- frontend/public/sw.js
+++
@@ -1,6 +0,0 @@
-self.addEventListener('install', () => { self.skipWaiting(); });
-self.addEventListener('activate', (e) => {
-  e.waitUntil(
-    caches.keys().then(n => Promise.all(n.map(k => caches.delete(k)))).then(() => clients.claim()).then(() => self.registration.unregister())
-  );
-});
